547ea2e9cd034cb32598f5d6c78b2b7ba1e10d53
Michael Leonhard <michael@leonhardllc.com>
2020-04-01 14:56:40 -0700

    Typo

--- modules_and_binaries/src/lib.rs
+++ modules_and_binaries/src/lib.rs
@@ -1,4 +1,4 @@
-// Cargo sees this lib.rs file and creates a default library crate, wiht the
+// Cargo sees this lib.rs file and creates a default library crate, with the
 // same name as the packages.  This lib.rs file defines the exports of the crate.
 pub mod multi_file_module;
 pub mod single_file_module;
