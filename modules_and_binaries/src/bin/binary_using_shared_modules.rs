26f48ac308525abee3778fc8e33b9fea02589ec2
Michael Leonhard <michael@leonhardllc.com>
2020-04-01 14:27:22 -0700

    Rename binary1 to binary_using_shared_modules.

--- modules_and_binaries/src/bin/binary_using_shared_modules.rs
+++ modules_and_binaries/src/bin/binary_using_shared_modules.rs
@@ -1,16 +1,19 @@
 // This is a single-file binary.
+
 // Each binary under src/bin/ is its own crate.
 // To use src/* modules, import the default library crate.  It has the same name as the package.
-extern crate modules_and_binaries;
-
 use modules_and_binaries::{module1, module2};
 
 fn main() {
     println!("{} {} {} {}",
-             module1::a(), module2::c(), module2::cde(), module2::nested::e());
-    // $ cargo run --bin binary1
+             module1::a(),
+             module2::c(),
+             module2::cde(),
+             module2::nested::e(),
+    );
+    // $ cargo run --bin binary_using_shared_modules
     // AB C CDE E
 
-    // $ cargo build --release && target/release/binary1
+    // $ cargo build --release && target/release/binary_using_shared_modules
     // AB C CDE E
 }
