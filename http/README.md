5056f597cb1be0c5da622a20c257823d25809423
Michael Leonhard <michael@leonhardllc.com>
2020-04-13 14:46:44 -0700

    Add streaming_response.

--- http/README.md
+++ http/README.md
@@ -3,11 +3,11 @@
 Opinion: Write HTTP servers and clients like [`opinion.rs`](src/bin/opinion.rs) which uses [`lib.rs`](src/lib.rs).
 
 That code does a lot of things.  Here are the things split into separate binaries:
+- [`graceful_shutdown.rs`](src/bin/graceful_shutdown.rs)
 - [`get.rs`](src/bin/get.rs)
 - [`streaming_response.rs`](src/bin/streaming_response.rs)
 - [`ipv4_and_ipv6.rs`](src/bin/ipv4_and_ipv6.rs)
 - [`port.rs`](src/bin/port.rs)
-- [`graceful_shutdown.rs`](src/bin/graceful_shutdown.rs)
 - [`request_id.rs`](src/bin/request_id.rs)
 
 More info:
--- http/src/bin/streaming_response.rs
+++ http/src/bin/streaming_response.rs
@@ -8,9 +8,15 @@ use tokio::stream::StreamExt as tokio_stream_StreamExt;
 
 async fn http_get(url: &str) {
     let url: hyper::Uri = url.parse().unwrap();
-    println!("get {}", url);
-    let mut response = Client::new().get(url).await.unwrap();
-    println!("result {}", response.status());
+    println!("Get {}", url);
+    let mut response = match Client::new().get(url).await {
+        Ok(response) => response,
+        Err(e) => {
+            println!("Error: {}", e);
+            return;
+        }
+    };
+    println!("Result {}", response.status());
     while let Some(chunk) = response.body_mut().data().await {
         match chunk {
             Ok(bytes) => println!("chunk {:?}", bytes),
@@ -20,46 +26,84 @@ async fn http_get(url: &str) {
             }
         }
     }
-    println!("read all chunks");
+    println!("Read all chunks");
 }
 
 async fn hello(_: Request<Body>) -> Result<Response<Body>, Infallible> {
     let top = futures::stream::once(async { String::from("Start:\n") });
-    let interval_numbers = futures::stream::StreamExt::enumerate(
-        tokio::time::interval(std::time::Duration::from_secs(1)).take(5))
-        .map(|(n, _val)| format!("{}\n", n));
+    let middle = {
+        let interval = tokio::time::interval(std::time::Duration::from_secs(1)).take(5);
+        let numbers = {
+            use futures::StreamExt;
+            interval.enumerate()
+        };
+        numbers.map(|(n, _val)| format!("{}\n", n))
+    };
     let bottom = futures::stream::once(async { String::from("Done.\n") });
-    let combined = top.chain(interval_numbers.chain(bottom));
+    let combined = top.chain(middle.chain(bottom));
     let as_results = combined.map(|s| Result::Ok::<Bytes, String>(Bytes::from(s)))
         //.chain(futures::stream::once(async { Result::Err(String::from("error1")) }))
         ;
     Ok(Response::new(Body::wrap_stream(as_results)))
 }
 
-#[tokio::main]
+pub async fn wait_for_stop_signal(stop_rx: tokio::sync::oneshot::Receiver<()>) {
+    // Handle TERM signal for running in Docker, Kubernetes, supervisord, etc.
+    // Also handle INT signal from CTRL-C in dev terminal.
+    use tokio::signal::unix::{signal, SignalKind};
+    let mut term_signal = signal(SignalKind::terminate())
+        .expect("Failed installing TERM signal handler");
+    let mut int_signal = signal(SignalKind::interrupt())
+        .expect("Failed installing INT signal handler");
+    futures::future::select(
+        stop_rx,
+        futures::future::select(term_signal.next(), int_signal.next()))
+        .await;
+    println!("Server stopping");
+}
+
+#[tokio::main]  // Sets up executor.  Waits for all tasks to finish before exiting the process.
 pub async fn main() -> () {
     let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 1690));
     let make_svc = make_service_fn(|_conn| {
         async { Ok::<_, Infallible>(service_fn(hello)) }
     });
-    let server = Server::bind(&addr).serve(make_svc);
+    let (stop_tx, stop_rx) = tokio::sync::oneshot::channel::<()>();
+    let server = Server::bind(&addr)
+        .serve(make_svc)
+        .with_graceful_shutdown(wait_for_stop_signal(stop_rx));
     println!("Listening on {}", &addr);
-    tokio::spawn(async move {
+    let server_handle = tokio::spawn(async move {
         server.await.unwrap();
+        println!("Server stopped.");
     });
 
-    http_get("http://127.0.0.1:1690").await;
+    // Client is streaming request when server starts shutdown.  Should finish streaming.
+    tokio::spawn(http_get("http://127.0.0.1:1690"));
+    std::thread::sleep(std::time::Duration::from_secs(1));
+    println!("Sending stop signal");
+    stop_tx.send(()).unwrap();
+    std::thread::sleep(std::time::Duration::from_secs(1));
+    http_get("http://127.0.0.1:1690").await;  // Connection refused.
+    server_handle.await.unwrap();
+    println!("Done.");
 
     // $ cargo run --bin streaming_response
     // Listening on 127.0.0.1:1690
-    // get http://127.0.0.1:1690/
-    // result 200 OK
+    // Get http://127.0.0.1:1690/
+    // Result 200 OK
     // chunk b"Start:\n"
     // chunk b"0\n"
+    // Sending stop signal
+    // Server stopping
     // chunk b"1\n"
+    // Get http://127.0.0.1:1690/
     // chunk b"2\n"
+    // Error: error trying to connect: tcp connect error: Connection refused (os error 61)
     // chunk b"3\n"
+    // Server stopped.
     // chunk b"4\n"
+    // Done.
     // chunk b"Done.\n"
-    // read all chunks
+    // Read all chunks
 }
