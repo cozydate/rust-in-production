e59fb263fcf44f6db93ff4b0df3c659c8323cb4b
Michael Leonhard <michael@leonhardllc.com>
2020-09-09 23:04:04 -0700

    Use TcpListener.accept() instead of next() (streams).

--- http/src/bin/tcp.rs
+++ http/src/bin/tcp.rs
@@ -1,98 +1,119 @@
-use std::sync::Arc;
+use std::future::Future;
+use std::pin::Pin;
+use std::println;
+use tokio::net::{TcpListener, TcpStream};
 
-use logging::{info, warn};
-
-#[derive(Debug)]
-pub struct HttpSession {
-    tcp_stream: tokio::net::TcpStream,
-    addr: std::net::SocketAddr,
+fn handle_conn(mut tcp_stream: TcpStream) -> Pin<Box<dyn Future<Output = ()> + Send>> {
+    Box::pin(async move {
+        use tokio::io::AsyncReadExt;
+        let mut buf = String::new();
+        match tcp_stream.read_to_string(&mut buf).await {
+            Ok(_) => {
+                println!("INFO server read {:?}", buf);
+            }
+            Err(e) => {
+                println!("WARN server read error: {:?}", e);
+                return;
+            }
+        };
+        println!("INFO server writing 'response'");
+        use tokio::io::AsyncWriteExt;
+        match tcp_stream.write_all(b"response").await {
+            Ok(_) => {}
+            Err(e) => {
+                println!("WARN server write error: {:?}", e);
+            }
+        };
+        match tcp_stream.shutdown(std::net::Shutdown::Write) {
+            Ok(_) => {}
+            Err(e) => {
+                println!("WARN server stream shutdown error: {:?}", e);
+            }
+        };
+    })
 }
 
-pub struct HttpServer {
-    listener: tokio::net::TcpListener,
-}
-
-impl HttpServer {
-    pub async fn next(&mut self) -> HttpSession {
-        loop {
-            match self.listener.accept().await {
-                Ok((tcp_stream, addr)) => {
-                    return HttpSession { tcp_stream, addr };
-                }
-                Err(e) => {
-                    warn!("Failed accepting connection from socket: {:?}", e);
-                    tokio::time::delay_for(std::time::Duration::from_secs(1)).
-                        await;
-                }
+// Note: We pass a fn that returns a Future which performs the actual handling.
+//       We can simplify this once Rust supports async closures:
+//       "Tracking issue for `#!feature(async_closure)]` (RFC 2394)"
+//       https://github.com/rust-lang/rust/issues/62290
+type HandlerFn = fn(tokio::net::TcpStream) -> Pin<Box<dyn Future<Output = ()> + Send>>;
+async fn accept_loop(mut listener: TcpListener, handler: HandlerFn) {
+    loop {
+        match listener.accept().await {
+            Ok((tcp_stream, addr)) => {
+                println!("INFO accepted {}", addr);
+                let f = handler(tcp_stream);
+                tokio::spawn(async {
+                    f.await;
+                });
+            }
+            Err(e) => {
+                println!("WARN Failed accepting connection from socket: {:?}", e);
+                tokio::time::delay_for(std::time::Duration::from_secs(1)).await;
             }
         }
     }
 }
 
-pub struct HttpServerBuilder {
-    all_interfaces: bool,
-    port: u16,
+async fn listen_on_all_interfaces(port: u16) -> tokio::io::Result<TcpListener> {
+    let interface =
+        std::net::IpAddr::from(std::net::Ipv6Addr::UNSPECIFIED /* includes ipv4 */);
+    let addr = std::net::SocketAddr::from((interface, port));
+    tokio::net::TcpListener::bind(&addr).await
 }
 
-impl HttpServerBuilder {
-    pub fn new(port: u16) -> HttpServerBuilder {
-        HttpServerBuilder {
-            all_interfaces: false,
-            port,
+async fn call_server(addr: &str) {
+    println!("INFO client connecting to {}", addr);
+    let mut tcp_stream = tokio::net::TcpStream::connect(addr).await.unwrap();
+    println!(
+        "INFO client connected to {}s",
+        tcp_stream.peer_addr().unwrap()
+    );
+    println!("INFO client writing 'greeting'");
+    {
+        use tokio::io::AsyncWriteExt;
+        match tcp_stream.write_all(b"greeting").await {
+            Ok(_) => {}
+            Err(e) => {
+                println!("WARN client write error: {:?}", e);
+            }
+        };
+    }
+    match tcp_stream.shutdown(std::net::Shutdown::Write) {
+        Ok(_) => {}
+        Err(e) => {
+            println!("WARN client stream shutdown-write error: {:?}", e);
         }
-    }
-
-    pub fn all_interfaces(self) -> HttpServerBuilder {
-        HttpServerBuilder {
-            all_interfaces: true,
-            port: self.port,
+    };
+    use tokio::io::AsyncReadExt;
+    let mut buf = String::new();
+    match tcp_stream.read_to_string(&mut buf).await {
+        Ok(_) => {
+            println!("INFO client read {:?}", buf);
         }
-    }
-
-    pub async fn run(self) -> std::io::Result<HttpServer> {
-        let interface =
-            if self.all_interfaces {
-                std::net::IpAddr::from(std::net::Ipv6Addr::UNSPECIFIED /* includes ipv4 */)
-            } else {
-                std::net::IpAddr::from(std::net::Ipv4Addr::LOCALHOST)
-            };
-        let addr = std::net::SocketAddr::from((interface, self.port));
-        info!("Listening for TCP connections on {}", addr);
-        let listener = tokio::net::TcpListener::bind(&addr).await?;
-        Ok(HttpServer { listener })
-    }
+        Err(e) => {
+            println!("WARN client read error: {:?}", e);
+            return;
+        }
+    };
 }
 
-struct Handler {}
-
-impl Handler {
-    async fn handle(&self, session: HttpSession) {
-        info!("Got {:?}", session)
-    }
-}
-
-pub async fn async_main() -> () {
-    let mut http_server = HttpServerBuilder::new(1690)
-        .all_interfaces()
-        .run()
-        .await.unwrap();
-    let handler = Arc::new(Handler {});
+async fn async_main() -> () {
+    let listener = listen_on_all_interfaces(1690).await.unwrap();
+    println!(
+        "INFO Listening for TCP connections on {}",
+        listener.local_addr().unwrap()
+    );
     tokio::spawn(async move {
-        loop {
-            let http_session = http_server.next().await;
-            let handler_clone = handler.clone();
-            tokio::spawn(async move {
-                handler_clone.handle(http_session).await;
-            });
-        }
+        accept_loop(listener, handle_conn).await;
     });
 
-    tokio::net::TcpStream::connect("127.0.0.1:1690").await.unwrap();
-    tokio::net::TcpStream::connect("::1:1690").await.unwrap();
+    call_server("127.0.0.1:1690").await;
+    call_server("[::1]:1690").await;
 }
 
 pub fn main() {
-    let _global_logger_guard = logging::configure("info").unwrap();
     let mut runtime = tokio::runtime::Builder::new()
         .threaded_scheduler()
         .enable_all()
@@ -102,3 +123,20 @@ pub fn main() {
     // Drops waiting tasks.  Waits for all busy tasks to await and drops them.  Gives up after timeout.
     runtime.shutdown_timeout(std::time::Duration::from_secs(3));
 }
+
+// $ cargo run --bin tcp
+// INFO Listening for TCP connections on [::]:1690
+// INFO client connecting to 127.0.0.1:1690
+// INFO client connected to 127.0.0.1:1690s
+// INFO client writing 'greeting'
+// INFO accepted [::ffff:127.0.0.1]:51298
+// INFO server read "greeting"
+// INFO server writing 'response'
+// INFO client read "response"
+// INFO client connecting to [::1]:1690
+// INFO client connected to [::1]:1690s
+// INFO client writing 'greeting'
+// INFO accepted [::1]:51299
+// INFO server read "greeting"
+// INFO server writing 'response'
+// INFO client read "response"
