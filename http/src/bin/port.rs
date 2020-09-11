0b4eb7b610ad6d79f0fc1126751885ac1ec1424f
Michael Leonhard <michael@leonhardllc.com>
2020-09-10 18:54:09 -0700

    Simplify bin/port.

--- http/src/bin/port.rs
+++ http/src/bin/port.rs
@@ -1,5 +1,6 @@
+// This program shows how to read the PORT environment variable.
+
 use std::println;
-use tokio::net::{TcpListener, TcpStream};
 
 pub fn parse_env_var<T>(name: &str, default: T) -> T
 where
@@ -17,37 +18,28 @@ where
     }
 }
 
-async fn handle_conn(mut tcp_stream: TcpStream) {
-    use tokio::io::AsyncWriteExt;
-    if let Err(e) = tcp_stream.write_all(b"response").await {
-        println!("WARN server write error: {:?}", e);
-        return;
-    }
-}
-
-async fn accept_loop(mut listener: TcpListener) {
-    loop {
-        match listener.accept().await {
-            Ok((tcp_stream, _addr)) => {
-                tokio::spawn(handle_conn(tcp_stream));
-            }
-            Err(e) => {
-                println!("WARN Failed accepting connection from socket: {:?}", e);
-                tokio::time::delay_for(std::time::Duration::from_secs(1)).await;
+async fn async_main() -> () {
+    let port: u16 = parse_env_var("PORT", 1690); // <--------------
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], port));
+    let mut listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    println!(
+        "INFO server listening on {}",
+        listener.local_addr().unwrap()
+    );
+    tokio::spawn(async move {
+        loop {
+            let (mut tcp_stream, _addr) = listener.accept().await.unwrap();
+            use tokio::io::AsyncWriteExt;
+            if let Err(e) = tcp_stream.write_all(b"response").await {
+                println!("WARN server write error: {:?}", e);
+                return;
             }
         }
-    }
-}
+    });
 
-async fn listen_on_all_interfaces(port: u16) -> tokio::io::Result<TcpListener> {
-    let interface =
-        std::net::IpAddr::from(std::net::Ipv6Addr::UNSPECIFIED /* includes ipv4 */);
-    let addr = std::net::SocketAddr::from((interface, port));
-    tokio::net::TcpListener::bind(&addr).await
-}
-
-async fn call_server(addr: &str) {
-    let mut tcp_stream = tokio::net::TcpStream::connect(addr).await.unwrap();
+    let mut tcp_stream = tokio::net::TcpStream::connect(&format!("127.0.0.1:{}", port))
+        .await
+        .unwrap();
     use tokio::io::AsyncReadExt;
     let mut buf = String::new();
     if let Err(e) = tcp_stream.read_to_string(&mut buf).await {
@@ -55,19 +47,6 @@ async fn call_server(addr: &str) {
         return;
     }
     println!("INFO client read {:?}", buf);
-}
-
-async fn async_main() -> () {
-    let port: u16 = parse_env_var("PORT", 1690); // <---------------------
-    let listener = listen_on_all_interfaces(port).await.unwrap();
-    println!(
-        "INFO server listening on {}",
-        listener.local_addr().unwrap()
-    );
-    tokio::spawn(accept_loop(listener));
-
-    call_server(&format!("127.0.0.1:{}", port)).await;
-    call_server(&format!("[::1]:{}", port)).await;
 }
 
 pub fn main() {
@@ -81,19 +60,15 @@ pub fn main() {
 }
 
 // $ cargo run --bin port
-// INFO server listening on [::]:1690
-// INFO client read "response"
+// INFO server listening on 127.0.0.1:1690
 // INFO client read "response"
 
 // $ PORT=1700 cargo run --bin port
-// INFO server listening on [::]:1700
-// INFO client read "response"
+// INFO server listening on 127.0.0.1:1700
 // INFO client read "response"
 
 // $ PORT= cargo run --bin port
-// thread 'main' panicked at 'Failed parsing env var PORT="": ParseIntError { kind: Empty }', src/bin/port.rs:12:14
-// note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+// thread 'main' panicked at 'Failed parsing env var PORT="": ParseIntError { kind: Empty }', src/bin/port.rs:11:14
 
 // $ PORT=abc cargo run --bin port
-// thread 'main' panicked at 'Failed parsing env var PORT="abc": ParseIntError { kind: InvalidDigit }', src/bin/port.rs:12:14
-// note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+// thread 'main' panicked at 'Failed parsing env var PORT="abc": ParseIntError { kind: InvalidDigit }', src/bin/port.rs:11:14
