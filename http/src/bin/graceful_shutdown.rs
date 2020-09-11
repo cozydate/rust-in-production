5859abb9d35ac1fa01dce34c8f7c0fb4f702a9df
Michael Leonhard <michael@leonhardllc.com>
2020-09-10 18:54:58 -0700

    Simplify bin/graceful_shutdown.

--- http/src/bin/graceful_shutdown.rs
+++ http/src/bin/graceful_shutdown.rs
@@ -1,3 +1,21 @@
+// This program shows how to shutdown a server that is serving clients.
+//
+// You can always just kill a server process.
+// - Requests with multiple steps will stop in the middle.
+//   - The server may leave data in an inconsistent state.
+//   - The server may repeat some operations, like sending an email.
+//   - When there are bugs, the server may skip operations or lose data.
+// - Clients doing big data uploads will get cut off and have to restart their
+//   uploads.  It's the same for large downloads.
+// - Log messages and metrics get lost.
+//
+// To prevent these problems, it's better to shut down the server gracefully:
+// 1. First stop accepting new connections.
+// 1. Signal all request handlers and background jobs to stop.
+//    They can reach a safe stopping place and then stop.
+// 1. Wait a bit to give them time to see the signal and respond.
+// 1. Finally, kill the server process.
+
 use std::println;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::mpsc;
@@ -72,37 +90,18 @@ pub fn new_stopper() -> (StopperController, Stopper) {
 }
 
 async fn handle_conn(mut tcp_stream: TcpStream, mut stopper: Stopper) {
-    println!("INFO server handling connection, writing response slowly");
-    delay_for(Duration::from_secs(1)).await;
-    if stopper.is_signalled() {
-        println!("INFO server shutting down, handle_conn returning early");
-        return;
-    }
-    println!("INFO server writing 'response1 '");
-    use tokio::io::AsyncWriteExt;
-    if let Err(e) = tcp_stream.write_all(b"response1 ").await {
-        println!("WARN server write error: {:?}", e);
-        return;
-    }
-    delay_for(Duration::from_secs(1)).await;
-    if stopper.is_signalled() {
-        println!("INFO server shutting down, handle_conn returning early");
-        return;
-    }
-    println!("INFO server writing 'response2 '");
-    if let Err(e) = tcp_stream.write_all(b"response2 ").await {
-        println!("WARN server write error: {:?}", e);
-        return;
-    }
-    delay_for(Duration::from_secs(1)).await;
-    if stopper.is_signalled() {
-        println!("INFO server shutting down, handle_conn returning early");
-        return;
-    }
-    println!("INFO server writing 'response3'");
-    if let Err(e) = tcp_stream.write_all(b"response3").await {
-        println!("WARN server write error: {:?}", e);
-        return;
+    println!("INFO handler writing response slowly");
+    for n in 0..3 {
+        delay_for(Duration::from_secs(1)).await;
+        if stopper.is_signalled() {
+            println!("INFO handler returning early because stopper is signalled");
+            return;
+        }
+        use tokio::io::AsyncWriteExt;
+        if let Err(e) = tcp_stream.write_all(n.to_string().as_bytes()).await {
+            println!("WARN server write error: {:?}", e);
+            return;
+        }
     }
 }
 
@@ -112,47 +111,25 @@ async fn accept_loop(mut listener: TcpListener, mut stopper: Stopper) {
             listen_result = listener.accept() => {
                 match listen_result {
                     Ok((tcp_stream, addr)) => {
-                        println!("INFO accepted {}", addr);
+                        println!("INFO server accepted connection {}", addr);
                         tokio::spawn(handle_conn(tcp_stream, stopper.clone()));
                     }
                     Err(e) => {
-                        println!("WARN Failed accepting connection from socket: {:?}", e);
+                        println!("WARN server error accepting connection: {:?}", e);
                         tokio::time::delay_for(std::time::Duration::from_secs(1)).await;
                     }
                 }
             },
             _ = stopper.wait() => {
-                println!("INFO exiting accept_loop");
+                println!("INFO server exiting accept_loop");
                 break
             },
         }
     }
 }
 
-async fn listen_on_all_interfaces(port: u16) -> tokio::io::Result<TcpListener> {
-    let interface =
-        std::net::IpAddr::from(std::net::Ipv6Addr::UNSPECIFIED /* includes ipv4 */);
-    let addr = std::net::SocketAddr::from((interface, port));
-    tokio::net::TcpListener::bind(&addr).await
-}
-
 async fn call_server(addr: &str) {
-    println!("INFO client connecting to {}", addr);
-    let mut tcp_stream = match tokio::net::TcpStream::connect(addr).await {
-        Ok(tcp_stream) => tcp_stream,
-        Err(e) => {
-            println!("WARN client connect error: {:?}", e);
-            return;
-        }
-    };
-    println!(
-        "INFO client connected to {}",
-        tcp_stream.peer_addr().unwrap()
-    );
-    if let Err(e) = tcp_stream.shutdown(std::net::Shutdown::Write) {
-        println!("WARN client stream shutdown-write error: {:?}", e);
-        return;
-    }
+    let mut tcp_stream = tokio::net::TcpStream::connect(addr).await.unwrap();
     use tokio::io::AsyncReadExt;
     loop {
         use bytes::BytesMut;
@@ -177,27 +154,21 @@ async fn call_server(addr: &str) {
 }
 
 async fn async_main() -> () {
-    let listener = listen_on_all_interfaces(1690).await.unwrap();
-    println!(
-        "INFO server listening on {}",
-        listener.local_addr().unwrap()
-    );
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 1690));
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    println!("INFO listening on {}", listener.local_addr().unwrap());
     let (mut stopper_controller, stopper) = new_stopper();
     tokio::spawn(accept_loop(listener, stopper));
-
-    tokio::spawn(async {
-        call_server("127.0.0.1:1690").await;
-        call_server("[::1]:1690").await;
-    });
+    tokio::spawn(call_server("127.0.0.1:1690"));
 
     // Comment this out to test ^C handling.
     tokio::spawn(async {
-        delay_for(Duration::from_secs(1)).await;
+        delay_for(Duration::from_millis(1500)).await;
         println!("INFO sending TERM signal to self");
         nix::sys::signal::kill(nix::unistd::getpid(), nix::sys::signal::SIGTERM).unwrap();
     });
 
-    println!("INFO main task waiting for stop signal");
+    println!("INFO main waiting for stop signal");
     wait_for_stop_signal().await;
     println!("INFO main got stop signal, signalling all tasks to stop");
     stopper_controller.signal_stop();
@@ -216,102 +187,45 @@ pub fn main() {
         .build()
         .unwrap();
     runtime.block_on(async_main());
-    // Drops waiting tasks.  Waits for all busy tasks to await and drops them.  Gives up after timeout.
-    runtime.shutdown_timeout(std::time::Duration::from_millis(1));
+    runtime.shutdown_background();
 }
 
 // $ cargo run --bin graceful_shutdown
-// INFO server listening on [::]:1690
-// INFO main task waiting for stop signal
-// INFO client connecting to 127.0.0.1:1690
-// INFO accepted [::ffff:127.0.0.1]:51972
-// INFO client connected to 127.0.0.1:1690
-// INFO server handling connection, writing response slowly
-// INFO server writing 'response1 '
+// INFO listening on 127.0.0.1:1690
+// INFO main waiting for stop signal
+// INFO server accepted connection 127.0.0.1:56383
+// INFO handler writing response slowly
+// INFO client read "0"
 // INFO sending TERM signal to self
-// INFO client read "response1 "
 // INFO main got stop signal, signalling all tasks to stop
 // INFO main waiting for tasks to stop
-// INFO exiting accept_loop
-// INFO server shutting down, handle_conn returning early
+// INFO server exiting accept_loop
+// INFO handler returning early because stopper is signalled
 // INFO exiting
 // INFO client connection EOF
-// INFO client connecting to [::1]:1690
-// WARN client connect error: Os { code: 61, kind: ConnectionRefused, message: "Connection refused" }
-// $
 
 // $ cargo run --bin graceful_shutdown
-// INFO server listening on [::]:1690
-// INFO main task waiting for stop signal
-// INFO client connecting to 127.0.0.1:1690
-// INFO accepted [::ffff:127.0.0.1]:51946
-// INFO client connected to 127.0.0.1:1690
-// INFO server handling connection, writing response slowly
-// INFO server writing 'response1 '
-// INFO client read "response1 "
-// INFO server writing 'response2 '
-// INFO client read "response2 "
-// INFO server writing 'response3'
-// INFO client read "response3"
-// INFO client connection EOF
-// INFO client connecting to [::1]:1690
-// INFO accepted [::1]:51947
-// INFO client connected to [::1]:1690
-// INFO server handling connection, writing response slowly
-// INFO server writing 'response1 '
-// INFO client read "response1 "
-// INFO server writing 'response2 '
-// INFO client read "response2 "
-// INFO server writing 'response3'
-// INFO client read "response3"
+// INFO listening on 127.0.0.1:1690
+// INFO main waiting for stop signal
+// INFO server accepted connection 127.0.0.1:56384
+// INFO handler writing response slowly
+// INFO client read "0"
+// ^CINFO main got stop signal, signalling all tasks to stop
+// INFO main waiting for tasks to stop
+// INFO server exiting accept_loop
+// INFO handler returning early because stopper is signalled
+// INFO exiting
+
+// $ cargo run --bin graceful_shutdown
+// INFO listening on 127.0.0.1:1690
+// INFO main waiting for stop signal
+// INFO server accepted connection 127.0.0.1:56385
+// INFO handler writing response slowly
+// INFO client read "0"
+// INFO client read "1"
+// INFO client read "2"
 // INFO client connection EOF
 // ^CINFO main got stop signal, signalling all tasks to stop
 // INFO main waiting for tasks to stop
-// INFO exiting accept_loop
+// INFO server exiting accept_loop
 // INFO exiting
-// $
-
-// $ cargo run --bin graceful_shutdown
-// INFO server listening on [::]:1690
-// INFO main task waiting for stop signal
-// INFO client connecting to 127.0.0.1:1690
-// INFO accepted [::ffff:127.0.0.1]:51948
-// INFO server handling connection, writing response slowly
-// INFO client connected to 127.0.0.1:1690
-// INFO server writing 'response1 '
-// INFO client read "response1 "
-// ^CINFO main got stop signal, signalling all tasks to stop
-// INFO main waiting for tasks to stop
-// INFO exiting accept_loop
-// INFO server shutting down, handle_conn returning early
-// INFO exiting
-// INFO client connection EOF
-// INFO client connecting to [::1]:1690
-// WARN client connect error: Os { code: 61, kind: ConnectionRefused, message: "Connection refused" }
-// $
-
-// $ cargo run --bin graceful_shutdown
-// INFO server listening on [::]:1690
-// INFO main task waiting for stop signal
-// INFO client connecting to 127.0.0.1:1690
-// INFO accepted [::ffff:127.0.0.1]:51944
-// INFO server handling connection, writing response slowly
-// INFO client connected to 127.0.0.1:1690
-// INFO server writing 'response1 '
-// INFO client read "response1 "
-// INFO server writing 'response2 '
-// INFO client read "response2 "
-// INFO server writing 'response3'
-// INFO client read "response3"
-// INFO client connection EOF
-// INFO client connecting to [::1]:1690
-// INFO client connected to [::1]:1690
-// INFO accepted [::1]:51945
-// INFO server handling connection, writing response slowly
-// ^CINFO main got stop signal, signalling all tasks to stop
-// INFO main waiting for tasks to stop
-// INFO exiting accept_loop
-// INFO server shutting down, handle_conn returning early
-// INFO exiting
-// INFO client connection EOF
-// $
